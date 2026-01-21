e5cc3b1b16713941b49edd262b1a79eda938e4e4
Sreemedhini <sreemedhini@gmail.com>
2026-01-21 11:06:26 +0530

    Add files via upload

---
+++ Subtraction of two numbers.r
@@ -0,0 +1,16 @@
+#Subtraction of two numbers using functions:
+sub_numbers<-function(x,y)
+{
+  return(x-y)
+}
+result1<-sub_numbers(10,5)
+result2<-sub_numbers(7,-3)
+result3<-sub_numbers(0,0)
+result4<-sub_numbers(3.5,2.1)
+
+#print results
+cat("Result of subtracting 5 and 10:",result1,"\n")
+cat("Result of subtracting -3 and 7:",result2,"\n")
+cat("Result of subtracting 0 and 0:",result3,"\n")
+cat("Result of subtracting 3.5 and 2.1:",result4,"\n")
+
