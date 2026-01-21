993a886627079df4f7cd0763371c8dcb9c6c626d
Sreemedhini <sreemedhini@gmail.com>
2026-01-21 14:44:04 +0530

    Add files via upload

---
+++ Multiplication of two numbers.r
@@ -0,0 +1,15 @@
+#Multiplying two numbers using functions:
+mul_numbers<-function(a,b)
+{
+  return(a*b)
+}
+result1<-mul_numbers(5,10)
+result2<-mul_numbers(-3,7)
+result3<-mul_numbers(0,0)
+result4<-mul_numbers(2.1,3.5)
+
+#print results
+cat("Result of multiplicating 5 and 10:",result1,"\n")
+cat("Result of multiplicating -3 and 7:",result2,"\n")
+cat("Result of multiplicating 0 and 0:",result3,"\n")
+cat("Result of multiplicating 2.1 and 3.5:",result4,"\n")
